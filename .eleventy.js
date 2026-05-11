39eddd9e91ce1d7c9b9423f37e79c005f0d08f81
nhsbsa-wicoo <wicoo@nhsbsa.nhs.uk>
2026-05-11 10:11:26 +0100

    update name

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,4 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("fonts");
-
-  return {
-    pathPrefix: "/a11y-resources/"
-  };
 };
